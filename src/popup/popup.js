bb560422bcb9f743e107abf7c0829faa2334ab2f
Kyle Crouse <crouse.kyle@gmail.com>
2019-06-18 20:47:33 -0600

    add Buefy

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import App from './App';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
+Vue.use(Buefy);
 
 global.browser = require('webextension-polyfill');
 Vue.prototype.$browser = global.browser;
@@ -7,6 +11,5 @@ Vue.prototype.$browser = global.browser;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-
   render: h => h(App),
 });
