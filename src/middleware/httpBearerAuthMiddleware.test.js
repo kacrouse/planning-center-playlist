807668934f6cf67a6310b93883f2b70951bc34de
Kyle Crouse <crouse.kyle@gmail.com>
2019-06-22 16:47:58 -0600

    eslint fixes

--- src/middleware/httpBearerAuthMiddleware.test.js
+++ src/middleware/httpBearerAuthMiddleware.test.js
@@ -1,19 +1,19 @@
-import httpBearerAuthMiddleware from "./httpBearerAuthMiddleware";
+import HttpBearerAuthMiddleware from './HttpBearerAuthMiddleware';
 
 describe('constructor', () => {
-    test('The token and name should be set.', () => {
-        const token = 'some auth token';
-        const middleware = new httpBearerAuthMiddleware(token);
-        expect(middleware.name).toEqual('HTTP_BEARER_AUTH');
-        expect(middleware.token).toEqual(token);
-    });
+  test('The token and name should be set.', () => {
+    const token = 'some auth token';
+    const middleware = new HttpBearerAuthMiddleware(token);
+    expect(middleware.name).toEqual('HTTP_BEARER_AUTH');
+    expect(middleware.token).toEqual(token);
+  });
 });
 describe('req', () => {
-    test('The Authorization header should be set if it doesn\'t exist', () => {
-        const token = 'some auth token';
-        const modifiedPayload = new httpBearerAuthMiddleware(token).req({ req: {} });
-        const authHeader = modifiedPayload.req.headers.Authorization;
-        expect(authHeader).toBeDefined();
-        expect(authHeader).toEqual(`Bearer ${token}`);
-    });
+  test("The Authorization header should be set if it doesn't exist", () => {
+    const token = 'some auth token';
+    const modifiedPayload = new HttpBearerAuthMiddleware(token).req({ req: {} });
+    const authHeader = modifiedPayload.req.headers.Authorization;
+    expect(authHeader).toBeDefined();
+    expect(authHeader).toEqual(`Bearer ${token}`);
+  });
 });
