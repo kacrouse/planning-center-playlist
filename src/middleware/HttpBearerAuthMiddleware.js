7801c0fd838d75f117330cdfd2dfb1ac5039adf2
Kyle Crouse <crouse.kyle@gmail.com>
2019-06-22 21:45:03 -0600

    bind req to this, populate headers if they aren't already there

--- src/middleware/HttpBearerAuthMiddleware.js
+++ src/middleware/HttpBearerAuthMiddleware.js
@@ -2,9 +2,11 @@ export default class {
   constructor(token) {
     this.token = token;
     this.name = 'HTTP_BEARER_AUTH';
+    this.req = this.req.bind(this);
   }
 
   req(payload) {
+    payload.headers = payload.headers || {};
     payload.headers.Authorization = `Bearer ${this.token}`;
     return payload;
   }
