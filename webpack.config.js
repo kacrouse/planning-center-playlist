b41f4c464bc2d20310e5a805e89bcdbc10e6cfae
Kyle Crouse <crouse.kyle@gmail.com>
2019-06-24 23:43:52 -0600

    allow vue dev tools to be used

--- webpack.config.js
+++ webpack.config.js
@@ -72,7 +72,7 @@ const config = {
           jsonContent.version = version;
 
           if (config.mode === 'development') {
-            jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
+            jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval' http://localhost:8098; object-src 'self'";
           }
 
           return JSON.stringify(jsonContent, null, 2);
