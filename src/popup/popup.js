e1a13de59746140c68066ec849bd4106fd0f9120
Kyle Crouse <crouse.kyle@gmail.com>
2019-07-02 21:41:18 -0600

    register hero component

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App';
 import CreateSpotifyPlaylistModal from '../components/CreateSpotifyPlaylistModal';
 import SpotifyPlaylistSelect from '../components/SpotifyPlaylistSelect';
+import Hero from '../components/Hero';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 
@@ -12,6 +13,7 @@ Vue.prototype.$browser = global.browser;
 
 Vue.component('create-spotify-playlist-modal', CreateSpotifyPlaylistModal);
 Vue.component('spotify-playlist-select', SpotifyPlaylistSelect);
+Vue.component('hero', Hero);
 
 /* eslint-disable no-new */
 new Vue({
