6ed04747e31a874369bd1acdcc88ae69882dadc2
Kyle Crouse <crouse.kyle@gmail.com>
2019-07-05 12:14:40 -0600

    add icon

---
+++ scripts/export-icon-sizes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+svgexport $1 $2/icon-16.png 3:3:19:19 16: "svg{fill:green}"
+svgexport $1 $2/icon-32.png 3:3:19:19 32: "svg{fill:green}"
+svgexport $1 $2/icon-48.png 3:3:19:19 48: "svg{fill:green}"
+svgexport $1 $2/icon-128.png 3:3:19:19 128: "svg{fill:green}"
