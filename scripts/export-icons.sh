a88e08cc07df692ef85c9a3adcc32ad1282b51d1
Kyle Crouse <crouse.kyle@gmail.com>
2019-07-05 14:28:28 -0600

    primary light color for extension icon

--- scripts/export-icons.sh
+++ scripts/export-icons.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-npx svgexport $1 $2/icon-16.png 3:3:19:19 16: "svg{fill:green}"
-npx svgexport $1 $2/icon-32.png 3:3:19:19 32: "svg{fill:green}"
-npx svgexport $1 $2/icon-48.png 3:3:19:19 48: "svg{fill:green}"
-npx svgexport $1 $2/icon-128.png 3:3:19:19 128: "svg{fill:green}"
+# todo: centralize primary color
+npx svgexport $1 $2/icon-16.png 3:3:19:19 16: "svg{fill:#87A9CC}"
+npx svgexport $1 $2/icon-32.png 3:3:19:19 32: "svg{fill:#87A9CC}"
+npx svgexport $1 $2/icon-48.png 3:3:19:19 48: "svg{fill:#87A9CC}"
+npx svgexport $1 $2/icon-128.png 3:3:19:19 128: "svg{fill:#87A9CC}"
